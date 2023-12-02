6d7f36344b54aa1fcda1bf32aa57a14c8322ec1f
remcarlmerza <remcarlmerza@gmail.com>
2023-12-02 22:54:28 +0800

    slider animation ( doesnt work)

---
+++ ML_ASP/wwwroot/js/slider.js
@@ -0,0 +1,10 @@
+﻿
+document.getElementById('next').onclick = funtion() {
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slider').appendChild(lists[1]);
+}
+
+document.getElementById('prev').onclick = funtion(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slider').prepend(lists[lists.length - 1]);
+}
